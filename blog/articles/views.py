454ac1706a4e949a0dd5759403aeb38c5d06587b
Pleshakov-Alexandr <sanya.pleshakov.95@mail.ru>
2023-04-20 23:43:37 +0300

    autorization and registration

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -2,6 +2,9 @@ from .models import Article
 from django.http import Http404
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
 
 
 def archive(request):
@@ -9,7 +12,6 @@ def archive(request):
 
 
 def create_post(request):
-
     if not request.user.is_anonymous:
         if request.method == "POST":
             form = {
@@ -31,6 +33,46 @@ def create_post(request):
             return render(request, 'create_post.html', {})
     else:
         raise Http404
+
+
+def registration(request):
+    if request.method == 'POST':
+        form = {
+            'username': request.POST["username"],
+            'password': request.POST["password"],
+            'email': request.POST["email"]
+        }
+        if len(form["username"]) >= 4 and len(form["password"]) >= 6:
+            try:
+                if User.objects.get(username=form["username"]) or User.objects.get(email=form["email"]):
+                    form['errors'] = u"Такой логин или почта уже существует"
+                    return render(request, 'registration.html', {'form': form})
+            except User.DoesNotExist:
+                User.objects.create_user(form["username"], form["email"], form["password"])
+                return redirect('/')
+        else:
+            form['errors'] = u"Логин должен содержать более 3 символов, а пароль более 5"
+            return render(request, 'registration.html', {'form': form})
+    else:
+        return render(request, 'registration.html', {})
+
+
+def authorisation(request):
+    if request.method == 'POST':
+        form = {
+            'username': request.POST["username"],
+            'password': request.POST["password"],
+        }
+        user = authenticate(username=form['username'], password=form['password'])
+
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            form['errors'] = u"Неверный логин или пароль"
+            return render(request, 'authorisation.html', {'form': form})
+    else:
+        return render(request, 'authorisation.html', {})
 
 
 def get_article(request, article_id):
